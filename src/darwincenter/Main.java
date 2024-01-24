8b55e10c1c750fadf2614998849051b037bb8b6c
jahirxtrap <jahirroman2002@gmail.com>
2024-01-24 10:18:22 -0500

    Arreglo

--- src/darwincenter/Main.java
+++ src/darwincenter/Main.java
@@ -1,7 +1,11 @@
 package darwincenter;
 
 import jade.core.Agent;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
 import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
 import jade.wrapper.StaleProxyException;
 
 /**
@@ -9,6 +13,23 @@ import jade.wrapper.StaleProxyException;
  * @author jahir
  */
 public class Main extends Agent {
+    
+    public static void main(String[] args) {
+        // Iniciar el contenedor principal
+        Runtime rt = Runtime.instance();
+        Profile p = new ProfileImpl();
+        ContainerController cc = rt.createMainContainer(p);
+
+        // Crear el agente principal y asignarlo al contenedor
+        try {
+            AgentController ac = cc.createNewAgent(
+                    "MainAgent",
+                    "darwincenter.Main",
+                    new Object[]{});
+            ac.start();
+        } catch (StaleProxyException e) { }
+
+    }
     
     @Override
     protected void setup() {
@@ -23,7 +44,7 @@ public class Main extends Agent {
         // Acciones de limpieza, si es necesario
     }
     
-    private void crearAgente(String agenteDestino) {
+    public void crearAgente(String agenteDestino) {
         try {
             AgentController ac = getContainerController().createNewAgent(
                     agenteDestino,
